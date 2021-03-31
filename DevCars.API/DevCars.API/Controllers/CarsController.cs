33c1e717df4d9390959e3056d1f1e9442d0c34c7
hugob <hugob@DESKTOP-2D092G7>
2021-03-31 20:43:32 -0300

    3# Live - Iniciando APIs

---
+++ DevCars.API/DevCars.API/Controllers/CarsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCars.API.Controllers
+{
+    [Route("api/customers")]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        //GET api/cars/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // SE O CARRO DE IDENTIFICADOR ID NÃO EXISTIR, RETORNA NOTFOUND
+            // SENAO, OK
+            return Ok();
+        }
+
+        //POST api/cars/1
+        [HttpPost]
+        public IActionResult Post([FromBody] )
+        {
+            // SE O CADASTRO FUNCIONAR, RETORNA CREATED (201)
+            // SE OS DADOS DE ENTRADA ESTIVEREM INCORRETOS, RETORNA BAD REQUEST(400)
+            // SE O CADASTRO FUNCIONAR, MAS NÃO TIVER UMA API DE CONSULTA, RETORNA NOCONTENT(204)
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put()
+        {
+            //SE A ATUALIZAÇÃO FUNCIONAR, RETORNA 204 NO CONTENT
+            //SE DADOS DE ENTRADA ESTIVEREM INCORRETOS, RETORNA 400 BAD REQUEST
+            return Ok();
+        }
+    }
+}
---
+++ DevCars.API/DevCars.API/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCars.API.Controllers
+{
+    [Route("api/customers")]
+    public class CustomersController: ControllerBase
+    {
+        //POST api/customers
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        //POST api/customers/2/orders
+        [HttpPost("{id}")]
+        public IActionResult Post (int id)
+        {
+            return Ok();
+        }
+
+        //PUT api/cars/1
+    }
+}
